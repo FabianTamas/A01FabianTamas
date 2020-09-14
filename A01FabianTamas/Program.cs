4380fcf6bff314a60fd18e07132e8075ebfe0271
FabianTamas <fabiant0130@gmail.com>
2020-09-14 11:06:01 +0200

    Beolvasas

--- A01FabianTamas/Program.cs
+++ A01FabianTamas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01FabianTamas
 {
@@ -10,6 +11,27 @@ namespace A01FabianTamas
     {
         static void Main(string[] args)
         {
+            Beolvasas();
+
+
+            Console.ReadKey();
+        }
+
+        private static void Beolvasas()
+        {
+            StreamReader adatok = new StreamReader("adatok.dat");
+            while (!adatok.EndOfStream)
+            {
+                string[] atmeneti = adatok.ReadLine().Split(' ');
+                int[] adat = new int[1000];
+                for (int i = 0; i < adat.Length; i++)
+                {
+                    adat[i] = int.Parse(atmeneti[i]);
+                    Console.Write("{0}", adat[i]);
+                }
+            }
+            adatok.Close();
+
         }
     }
 }
