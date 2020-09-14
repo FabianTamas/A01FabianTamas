7f94ef2f8643ead4354549337c1e7d982c8c9089
FabianTamas <fabiant0130@gmail.com>
2020-09-14 11:10:04 +0200

    Maximumertek

--- A01FabianTamas/Program.cs
+++ A01FabianTamas/Program.cs
@@ -12,9 +12,21 @@ namespace A01FabianTamas
         static void Main(string[] args)
         {
             Beolvasas();
-
+            Console.WriteLine("A maximum: {0}", Maximumertek());
 
             Console.ReadKey();
+        }
+
+        private static void Maximumertek()
+        {
+            int max = 0;
+            for (int i = 0; i < adat.Length; i++)
+            {
+                if (adat[i]>max)
+                {
+                    max = adat[i];
+                }
+            }
         }
 
         private static void Beolvasas()
